751dd51a2c5f01040dad984b4486d8ce1d8ee3f5
DochYaka <dmitrijkiselev194@gmail.com>
2026-04-06 23:17:42 +0700

    Добавлено: по нажатии кнопки "Вход" производится сборка пользователя

---
+++ TMP_Laba3/AssemblyPerson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMP_Laba3
+{
+    public class AssemblyPerson
+    {
+        public Person Assembly(string name, string password)
+        {
+            return new Person(name, password);
+        }
+    }
+}
---
+++ TMP_Laba3/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMP_Laba3
+{
+    public class Person
+    {
+        public string _name { get; private set; }
+        public string _password { get; private set; }
+
+        public Person(string name, string password)
+        {
+            _name = name; 
+            _password = password;
+        }
+    }
+}
