1f8185472d3d83c2050f2f0d982acbb626edc6c6
DochYaka <dmitrijkiselev194@gmail.com>
2026-04-07 12:55:50 +0700

    переделал Seek

--- TMP_Laba3/Seeker.cs
+++ TMP_Laba3/Seeker.cs
@@ -1,15 +1,31 @@
-﻿using TMP_Laba3_Libraries.Authorization;
+﻿using System.IO;
+using System.Windows.Documents;
+using TMP_Laba3_Libraries.Authorization;
 
 namespace TMP_Laba3
 {
     public class Seeker
     {
-        public Person? Seek(string name, string password, List<Person> users)
+        AssemblyPerson assembly = new AssemblyPerson();
+
+        public Person? Seek(string name, string password, string path)
         {
-            foreach (var user in users)
+            if (!File.Exists(path))
+                throw new FileNotFoundException();
+
+            foreach (var line in File.ReadAllLines(path))
             {
-                if (name == user.Name && password == user.Password)
-                    return user;
+                var parts = line.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length < 3)
+                    continue;
+
+                var _name = parts[0];
+                var _password = parts[1];
+                var role = parts[2];
+
+                if (name == _name && password == _password)
+                    return assembly.Assembly(_name, _password, role);
             }
             return null;
         }
